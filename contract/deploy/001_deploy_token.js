b3a84854695748deef7082247403b8d1309fb4a5
hsienfu <hsienfu.tseng@gmail.com>
2021-04-12 13:45:10 +0800

    fix deploy scripts

--- contract/deploy/001_deploy_token.js
+++ contract/deploy/001_deploy_token.js
@@ -1,9 +1,12 @@
 const func = async ({ getNamedAccounts, deployments, network }) => {
-  const { deploy } = deployments;
-  const { deployer } = await getNamedAccounts();
+  const { deploy, execute } = deployments;
+  const { deployer, governor } = await getNamedAccounts();
 
   // YF Token
   await deploy('YFToken', { from: deployer });
+
+  // NOTICE: 上线时要确认分配
+  await execute('YFToken', { from: deployer }, 'mint', governor, ethers.utils.parseUnits('500'));
 
   console.log('1. YF Token has deployed');
   return network.live;
--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -11,6 +11,37 @@ task("accounts", "Prints the list of accounts", async () => {
   }
 });
 
+task("yfpool", "Prints the info of YF pool", async () => {
+  const Pair    = await ethers.getContractFactory("contracts/mock/MockMdexPair.sol:MdexPair");
+  const YFToken = await ethers.getContractFactory("YFToken");
+  const YFPool  = await ethers.getContractFactory("YFPool");
+  const StratX  = await ethers.getContractFactory("StratX");
+
+  const yPool = new ethers.Contract('0x78117e80A887e03C1F57B0975bC9a798Cc29eeF7', YFPool.interface, ethers.provider);
+  const poolLength = await yPool.poolLength().then(n => n.toNumber());
+
+  for (let i = 0; i < poolLength; i++) {
+    const poolInfo = await yPool.poolInfo(i);
+    const pair  = new ethers.Contract(poolInfo.want, Pair.interface, ethers.provider);
+    const strat = new ethers.Contract(poolInfo.strat, StratX.interface, ethers.provider);
+
+    if (await strat.isErc20Token()) {
+      const symbol = await (new ethers.Contract(poolInfo.want, YFToken.interface, ethers.provider)).symbol();
+      console.log('pid = %s, want = %s, symbol = %s', i, poolInfo.want, symbol);
+    } else {
+      const token0 = await pair.token0();
+      const token1 = await pair.token1();
+
+      const symbol0 = await (new ethers.Contract(token0, YFToken.interface, ethers.provider)).symbol();
+      const symbol1 = await (new ethers.Contract(token1, YFToken.interface, ethers.provider)).symbol();
+
+      console.log('\n\n   pid = %s, want = %s', i, poolInfo.want);
+      console.log('token0 = %s symbol0 = %s', token0, symbol0);
+      console.log('token1 = %s symbol1 = %s', token1, symbol1);
+    }
+  }
+});
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
@@ -52,7 +83,11 @@ task("accounts", "Prints the list of accounts", async () => {
       url: 'https://bsc-dataseed1.ninicoin.io'
     },
     heco: {
+      accounts: {
+        mnemonic: "test test test"
+      },
       chainId: 128,
+      gasMultiplier: 1.25,
       gas: 'auto',
       gasPrice: 'auto',
       url: 'https://http-mainnet-node.huobichain.com',
